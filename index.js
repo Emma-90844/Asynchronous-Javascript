cb080384d4d7dd1022b9202baa6b467df4066de3
Emma <enabojohn@gmail.com>
2020-03-23 16:40:50 +0300

    Modified notes

--- index.js
+++ index.js
@@ -4,11 +4,41 @@ getUser(1, (user) =>{
     console.log('User', user);
 });
 //Get repositories function
-getRepositories("Dollaclan", (repositories) => {
+getRepositories("clan", (repositories) => {
     console.log('Repositories',repositories );
 });
 
 console.log('After');
+
+
+
+//SOLVING THE CALL BACK HELL
+//Creating a function out of the anonimpous function and passing
+// it as a parameter
+getUser(1, (user) => {
+    getRepositories(user.gitHubUsername, (repos) => {
+        getCommits(repo, displayCommits);
+    });
+});
+function User(user){
+
+}
+
+function displayCommits(commits){
+    console.log(commits);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Callbacks
 
@@ -21,7 +51,7 @@ function getUser (id, callback){
         console.log('Reading a user from the data base');
         callback({id: id, gitHubUsername: "Allen" }); // Call back and give it this user object
         }, 2000);
-}
+} 
 
 
 function getRepositories(username, callback){
