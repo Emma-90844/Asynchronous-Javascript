ca0d535c71291819b92d11e2da1862ce6619baeb
Emma <enabojohn@gmail.com>
2020-03-23 13:59:24 +0300

    Converted getRepositories function from sync to Asyc function

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 console.log('Before');
 //This an example of a non blocking function
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) =>{
+    console.log('User', user);
+});
+//Get repositories function
+getRepositories("Dollaclan", (repositories) => {
+    console.log('Repositories',repositories );
+});
+
 console.log('After');
 
 //Callbacks
+
 //Promisses
-//Sync and await
 
+//async and await
 
-
-function getUser (id, callbacl){
+function getUser (id, callback){
     setTimeout(() => {
         console.log('Reading a user from the data base');
-        return {id: id, gitHubUsername: "Allen" };
+        callback({id: id, gitHubUsername: "Allen" }); // Call back and give it this user object
         }, 2000);
 }
+
+
+function getRepositories(username, callback){
+    setTimeout(() => {
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+   
+}
+
 
 //NOTE
 /*Whenever you are running an operation that involves disk and 
 network access you are dealing with asynchronous programing
 
 A callback is a function that is called when the results of 
-asynchronious function is ready
+asynchronious function is ready.
 */
 
 
